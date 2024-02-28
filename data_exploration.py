2562f2fc9f2e10106d6fa361f030044a84050982
f-wright <fwright@hmc.edu>
2024-02-27 20:56:40 -0800

    setup

--- data_exploration.py
+++ data_exploration.py
@@ -14,9 +14,16 @@ task = "tc"
 def load_data():
     pdf_train_labels = pd.read_csv(train_articles_flc_tc_labels, sep="\t", header=None, names=["article", "label", "start_i", "end_i"])
     
-    pdf_train_labels['article'] = pdf_train_labels['article'].apply(lambda x: int(str(x)[-4:]))
+    pdf_train_labels['article'] = pdf_train_labels['article'].apply(lambda x: "dev-articles/article" + str(x) + ".txt")
+    pdf_train_labels['article'] = pdf_train_labels['article'].apply(lambda x: os.path)
 
+    print(pdf_train_labels)
 
+def get_segment(filename: str, start_i: int, end_i, int):
+    with open(filename, "r") as fp:
+        lines = fp.readlines()
+        print(lines)
+    
 
 
 if __name__ == "__main__":
---
+++ sources.md
@@ -0,0 +1,2 @@
+https://medium.com/@jihwangk/fine-grained-propaganda-detection-and-classification-with-bert-dfad4acaa321
+https://medium.com/analytics-vidhya/6-steps-to-build-roberta-a-robustly-optimised-bert-pretraining-approach-e508ebe78b96
