2ed5ac7b18c13690ea4c49c05a65d43c5c761b88
sophiee-b <sophie.bekerman@gmail.com>
2024-02-29 13:54:35 -0800

    calculate f1 score of bert outputs

---
+++ get_metrics.py
@@ -0,0 +1,18 @@
+import csv
+from sklearn.metrics import f1_score
+
+y_true = []
+with open("datasets/dev-task-flc-tc.labels", "r") as f:
+    reader = csv.reader(f, delimiter="\t")
+    for row in reader:
+        y_true += [row[1]]
+
+y_pred = []
+with open("output/output", "r") as f:
+    reader = csv.reader(f, delimiter="\t")
+    for row in reader:
+        y_pred += [row[1]]
+
+
+print(f1_score(y_true, y_pred, average='weighted'))
+# current bert model has an F1 score of 0.11 :((
