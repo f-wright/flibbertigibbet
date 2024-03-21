68e811c5648df27e291056326ddfa4aac9c7d43c
f-wright <fwright@hmc.edu>
2024-03-20 17:28:21 -0700

    updated to get f1s for multiple runs

--- get_metrics.py
+++ get_metrics.py
@@ -1,18 +1,35 @@
 import csv
+import os
+
 from sklearn.metrics import f1_score
 
-y_true = []
-with open("datasets/dev-task-flc-tc.labels", "r") as f:
-    reader = csv.reader(f, delimiter="\t")
-    for row in reader:
-        y_true += [row[1]]
 
-y_pred = []
-with open("output/output", "r") as f:
-    reader = csv.reader(f, delimiter="\t")
-    for row in reader:
-        y_pred += [row[1]]
+def get_f1(folder):
+    y_true = []
+    with open("datasets/dev-task-flc-tc.labels", "r") as f:
+        reader = csv.reader(f, delimiter="\t")
+        for row in reader:
+            y_true += [row[1]]
+
+    y_pred = []
+    path = os.path.join("_output", folder, "output")
+    with open(path, "r") as f:
+        reader = csv.reader(f, delimiter="\t")
+        for row in reader:
+            y_pred += [row[1]]
+
+    return f1_score(y_true, y_pred, average="weighted")
 
 
-print(f1_score(y_true, y_pred, average="weighted"))
-# current bert model has an F1 score of 0.11 :((
+folders = [
+    "_epoch_1",
+    "_epoch_3",
+    "_epoch_5",
+    "seq_len_64",
+    "seq_len_128",
+    "seq_len_256",
+    "seq_len_512",
+]
+
+for folder in folders:
+    print("folder", folder, "has f1", get_f1(folder))
